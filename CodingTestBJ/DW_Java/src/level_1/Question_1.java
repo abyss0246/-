ee55a1317a38c7ce96d71a43a5ea9c7d07b1e8a7
abyss0246 <akatsukiblue1@gmail.com>
2024-11-16 20:51:06 +0900

    연동 커밋테스트

--- CodingTestBJ/DW_Java/src/level_1/Question_1.java
+++ CodingTestBJ/DW_Java/src/level_1/Question_1.java
@@ -4,6 +4,7 @@ public class Question_1 {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+		System.out.println("이러면 어떻게 되나?");
 	}
 	//입출력 가장 기초의 문제, 시스템아웃.프린트라인으로 "Hello World!"를 출력!
 }
